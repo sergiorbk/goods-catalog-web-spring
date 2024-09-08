31000deb2cfcc5547b0f80311bfe3651efbf2598
sergiorbk <sergey24rybak@gmail.com>
2024-09-08 03:48:21 +0300

    Product services initial commit

--- src/main/java/com/sergosoft/goodscatalog/service/ProductService.java
+++ src/main/java/com/sergosoft/goodscatalog/service/ProductService.java
@@ -1,5 +1,14 @@
 package com.sergosoft.goodscatalog.service;
 
+import com.sergosoft.goodscatalog.model.Product;
+
+import java.util.List;
+
 public interface ProductService {
 
+//    List<Product> getAllProducts(); TODO
+//    Product getProductById(Long id);
+//    Product createProduct(Product product);
+//    Product updateProduct(Long id, Product product);
+//    boolean deleteProduct(Long id);
 }
