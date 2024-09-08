5d0ec50ad56ac8ad5febd86b0c030ab78fb53270
sergiorbk <sergey24rybak@gmail.com>
2024-09-08 03:19:33 +0300

    Initial commit

---
+++ src/main/java/com/sergosoft/goodscatalog/GoodsCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.sergosoft.goodscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsCatalogApplication.class, args);
+	}
+
+}
