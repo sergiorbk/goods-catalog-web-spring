12619f8ff358e9f608e53370929f5af9e7fa5d19
sergiorbk <sergey24rybak@gmail.com>
2024-09-09 19:22:42 +0300

    ProductController hotfix;

--- src/main/java/com/sergosoft/goodscatalog/SampleDataLoader.java
+++ src/main/java/com/sergosoft/goodscatalog/SampleDataLoader.java
@@ -10,7 +10,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class SampleDataLoader implements ApplicationRunner {
