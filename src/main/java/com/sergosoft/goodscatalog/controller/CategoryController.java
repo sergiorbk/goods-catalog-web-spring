ffd7d2e9696117547708b16db1cdebcc149f1f26
sergiorbk <sergey24rybak@gmail.com>
2024-09-09 19:24:05 +0300

    Controllers request mapping hotfix x2;

--- src/main/java/com/sergosoft/goodscatalog/controller/CategoryController.java
+++ src/main/java/com/sergosoft/goodscatalog/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package com.sergosoft.goodscatalog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/category")
+@Controller
+@RequestMapping("/categories")
 public class CategoryController {
 
     // TODO
--- src/main/java/com/sergosoft/goodscatalog/controller/ProductController.java
+++ src/main/java/com/sergosoft/goodscatalog/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
