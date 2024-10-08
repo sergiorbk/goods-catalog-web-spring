aa898842b1fd243e5e8c1ed9f816eec4d58cf0e3
sergiorbk <sergey24rybak@gmail.com>
2024-10-08 15:01:16 +0300

    @Query request existsByName for category repository was implemented

--- src/main/java/com/sergosoft/goodscatalog/repository/CategoryRepository.java
+++ src/main/java/com/sergosoft/goodscatalog/repository/CategoryRepository.java
@@ -2,12 +2,13 @@ package com.sergosoft.goodscatalog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import com.sergosoft.goodscatalog.model.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer>, JpaSpecificationExecutor<Category> {
 
+    @Query("SELECT COUNT(c) > 0 FROM Category c WHERE c.name = :name")
     boolean existsByName(String name);
 }
